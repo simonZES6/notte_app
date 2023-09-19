a92432b3926d7c0963c1f260ec79d5a0946e95b5
simonZES6 <145031017+simonZES6@users.noreply.github.com>
2023-09-19 10:51:46 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,49 +5,79 @@ from pydantic import BaseModel
 
 app = FastAPI()
 data =[]
+Category_data=[]
 # array waar data opgeslagen moet worden
-class note_Data:
-    def note():
-        data = []
 
+class Items(BaseModel):
+    category_id:int
+    name: str
+    description: str
+class Category(Items):
+    Category_id: int
+    name:str
+    description:str
+    date:str
 
-class Note(BaseModel):
+class Note(Items):
     note_id :int
     user_id :int
-    name :str
+    name  :str
     lastname :str
     titel : str
     chapter :str
-    paragraph :str
+    paragraph:str
+    category_id: int
+
 
 #opslaan van notes
-@app.post("/notes")
-def read_user(note: Note):
-   data.append(note)
+@app.post("/notes/category")
+def read_user(note: Note,category:Category):
+   data.append(note,category)
    return {"massage":"Notitie toegevoegd"}
 
 #ophalen van notes
-@app.get("/notes")
-def get_notes(note: Note):
-    data.append(note)
-    return {"massage":"Notitie opgehaald"}
+@app.get("/notes/category")
+def get_notes(note: Note,category:Category):
+    return data
+ 
+#ophalen van category
+@app.get("/category")
+def get_category(category:Category):
+    return data 
+
 
 
 #updaten van notes
+note = []
 @app.put("/notes")
 def update_notes(note: Note):
-   data.append(note)
-   return{"massage": "Notities bijgewerkt"}
+    for note in note:
+        if  note.note_id ==note.note_id:
+            note.note_id= "0"
+            note.user_id= note.user_id
+            note.name = note.name
+            note.lastname = note.lastname
+            note.titel= note.titel
+            note.chapter= note.chapter
+            note.paragraph=note.paragraph
+            
+        return{"massage": "Notities bijgewerkt"} 
 
 
 #verwijderen van notes
+
 @app.delete("/notes")
 def delete_note(note:Note):
-    data.remove(note)
-    return{"massage":"notities verwijderd"}
+    for request_data in note:
+        if note.note_id == note.note_id:
+           data.remove(note)
+
+  
 
 
-
+class GoldenRetriver(Dog):
+    def speak(self, sound="bark"):
+        return super().speak(sound)
 
 
 
